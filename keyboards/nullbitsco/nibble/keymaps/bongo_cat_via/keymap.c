76e6f5a4e18056e78922c4cabaedd1e2a0b61c10
William Randall <15082470+randallwc@users.noreply.github.com>
2025-12-07 11:52:28 +0000

    [working] bongo cat via

--- keyboards/nullbitsco/nibble/keymaps/bongo_cat_via/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/bongo_cat_via/keymap.c
@@ -16,6 +16,8 @@
 
 #include QMK_KEYBOARD_H
 #include "animation_frames.h"
+#include "oled/oled_driver.h"
+#include "wpm.h"
 
 enum layer_names {
   _BASE,
@@ -24,38 +26,26 @@ enum layer_names {
   _VIA3
 };
 
-#define KC_DISC_MUTE KC_F23
-#define KC_DISC_DEAF KC_F24
-
 enum custom_keycodes {
-  PROG = QK_KB_0,
-  DISC_MUTE,
-  DISC_DEAF,
-  SUPER_ALT_TAB,
+  PROG = SAFE_RANGE,
   _NUM_CUST_KCS,
 };
 
-// Macro variables
-bool is_alt_tab_active = false;
-uint16_t alt_tab_timer = 0;
-bool muted = false;
-bool deafened = false;
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all(
-              QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
-    KC_F13,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-    KC_F14,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
-    KC_F15,   KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_F16,   KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                  MO(_VIA1), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+              QK_GESC,  KC_1,    KC_2,    KC_3, KC_4,  KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,
+    KC_MUTE,   KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,  KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+    MO(_VIA1), KC_CAPS, KC_A,    KC_S,    KC_D, KC_F,  KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    KC_BRIU,   KC_LSFT, KC_NUBS, KC_Z,    KC_X, KC_C,  KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    KC_BRID,   KC_LCTL, KC_LALT, KC_LGUI,              KC_SPC,               MO(_VIA1), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_VIA1] = LAYOUT_all(
-              QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_END,
-    RGB_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
+              QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+    UG_TOGG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______,          _______, KC_HOME,
+    _______,  _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END,
+    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
   ),
 
   [_VIA2] = LAYOUT_all(
@@ -200,51 +190,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
         break;
 
-        case DISC_MUTE:
-          if (record->event.pressed) {
-            tap_code(KC_DISC_MUTE);
-            if (!rgblight_is_enabled()) break;
-
-            if (muted) {
-              rgblight_enable_noeeprom();
-            } else {
-              rgblight_timer_disable();
-              uint8_t val = rgblight_get_val();
-              rgblight_sethsv_range(255, 255, val, 0, 1);
-            }
-            muted = !muted;
-          }
-        break;
-
-        case DISC_DEAF:
-          if (record->event.pressed) {
-            tap_code(KC_DISC_DEAF);
-            if (!rgblight_is_enabled()) break;
-
-            if (deafened) {
-              rgblight_enable_noeeprom();
-            } else {
-              rgblight_timer_disable();
-              uint8_t val = rgblight_get_val();
-              rgblight_sethsv_range(255, 255, val, 0, RGBLED_NUM-1);
-            }
-            deafened = !deafened;
-          }
-        break;
-
-        case SUPER_ALT_TAB:
-          if (record->event.pressed) {
-            if (!is_alt_tab_active) {
-              is_alt_tab_active = true;
-              register_code(KC_LALT);
-            }
-            alt_tab_timer = timer_read();
-            register_code(KC_TAB);
-          } else {
-            unregister_code(KC_TAB);
-          }
-          break;
-
         default:
         break;
     }
@@ -252,11 +197,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+// Initialize remote keYBoard, if connected
+void matrix_init_user(void) {
+    matrix_init_remote_kb();
+}
+
+// Scan and parse keystrokes from remote keyboard, if connected
 void matrix_scan_user(void) {
-  if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 1000) {
-      unregister_code(KC_LALT);
-      is_alt_tab_active = false;
-    }
-  }
+    matrix_scan_remote_kb();
 }
